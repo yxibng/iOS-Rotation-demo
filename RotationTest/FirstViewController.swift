a1c37fb4b6de27a337c913d8806f0cd1605feaa8
yxibng <yxibng@gmail.com>
2020-04-04 21:32:12 +0800

    Initial Commit

---
+++ RotationTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RotationTest
+//
+//  Created by yxibng on 2020/4/4.
+//  Copyright © 2020 yxibng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RotationTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RotationTest
+//
+//  Created by yxibng on 2020/4/4.
+//  Copyright © 2020 yxibng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
